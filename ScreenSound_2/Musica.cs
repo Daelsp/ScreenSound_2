5bae62fcbd530cb624feda9a23782e409cbe54f3
Daniel <dsilvapinto949@gmail.com>
2023-10-29 21:07:01 -0300

    first commit

---
+++ ScreenSound_2/Musica.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Channels;
+
+class Musica
+{
+    public Musica(Banda artista, string nome)
+    {
+        Artista = artista;
+        Nome = nome;
+    }
+
+    public string Nome { get; }
+    public Banda Artista { get;  }
+    public int Duracao { get; set; }
+    public bool Disponivel { get; set; }
+    public string DescricaoResumida => $"A música {Nome} pertence a banda {Artista}";
+    public Genero Genero { get; set; }
+
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Artista: {Artista.Nome}");
+        Console.WriteLine($"Duração: {Duracao}");
+        if( Disponivel )
+        {
+            Console.WriteLine("Disponivel no plano.");
+        }
+        else
+        {
+            Console.WriteLine("Adiquira o plano Plus+.");
+        }
+    }
+}
+
---
+++ ScreenSound_2/Program.cs
@@ -0,0 +1,15 @@
+﻿Episodio ep1 = new(2, "Técnicas de facilitação", 45);
+ep1.AdicionarConvidados("Maria");
+ep1.AdicionarConvidados("Marcelo");
+Console.WriteLine(ep1.Resumo);
+
+Episodio ep2 = new(1, "Técnicas de aprendizado", 60);
+ep1.AdicionarConvidados("Dael");
+ep1.AdicionarConvidados("Lucas");
+ep2.AdicionarConvidados("Marcos");
+Console.WriteLine(ep1.Resumo);
+
+Podcast podcast = new("Podcast especial", "Daniel");
+podcast.AdicionarEpisodio(ep1);
+podcast.AdicionarEpisodio(ep2);
+podcast.ExibirDetalhes();
